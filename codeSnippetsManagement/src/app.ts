ee190026fe4ac6cdb4e5c52494fd8fefa1c71255
carlosEA28 <carloseaduarte28@gmail.com>
2025-12-23 14:14:36 -0300

    feat: add scalar

--- codeSnippetsManagement/src/app.ts
+++ codeSnippetsManagement/src/app.ts
@@ -1,9 +1,18 @@
 import express from "express";
 import { userRoutes } from "./routes/user.js";
 import { snippetRoutes } from "./routes/snippet.js";
+import { apiReference } from "@scalar/express-api-reference";
 
 export const app = express();
 
 app.use(express.json());
+app.use(
+  "/reference",
+  apiReference({
+    theme: "purple",
+    url: "/openapi.json",
+  }),
+);
+
 app.use(userRoutes);
 app.use(snippetRoutes);
